ea8d09be70c8d90ae7140cb44473411d605b47fc
rizki <82581582+rizukiii@users.noreply.github.com>
2025-01-28 11:45:12 +0700

    first commit

---
+++ resources/views/backend/partials/head.blade.php
@@ -0,0 +1,26 @@
+@push('head')
+
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>@yield('title')</title>
+        <!-- plugins:css -->
+        <link rel="stylesheet" href="{{ asset('corona') }}/template/assets/vendors/mdi/css/materialdesignicons.min.css">
+        <link rel="stylesheet" href="{{ asset('corona') }}/template/assets/vendors/css/vendor.bundle.base.css">
+        <!-- endinject -->
+        <!-- Plugin css for this page -->
+        <link rel="stylesheet" href="{{ asset('corona') }}/template/assets/vendors/jvectormap/jquery-jvectormap.css">
+        <link rel="stylesheet" href="{{ asset('corona') }}/template/assets/vendors/flag-icon-css/css/flag-icon.min.css">
+        <link rel="stylesheet" href="{{ asset('corona') }}/template/assets/vendors/owl-carousel-2/owl.carousel.min.css">
+        <link rel="stylesheet"
+            href="{{ asset('corona') }}/template/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
+        <!-- End plugin css for this page -->
+        <!-- inject:css -->
+        <!-- endinject -->
+        <!-- Layout styles -->
+        <link rel="stylesheet" href="{{ asset('corona') }}/template/assets/css/style.css">
+        <!-- End layout styles -->
+        <link rel="shortcut icon" href="Coat_of_arms_of_Yogyakarta.svg.png" />
+    </head>
+@endpush
